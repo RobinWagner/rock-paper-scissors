453a12c9bfae9c6e52d3ca21091194dab2093bd1
Robin <robin-wagner@web.de>
2019-06-29 21:01:13 +0200

    Move files

--- game.js
+++ game.js
@@ -2,28 +2,31 @@
 let playerScore = 0;
 let computerScore = 0;
 const numberOfRounds = 5;
+const maximumScore = 5;
+
+const displayedPlayerScore = document.querySelector('#player-score');
+const displayedComputerScore = document.querySelector('#computer-score');
+const displayedResult = document.querySelector('#result');
 
 function game() {
   let maximumScore = 0;
   displayWelcomeMessage();
-  while (maximumScore < numberOfRounds) {
-    let playerSelection = playerPlay();
-    let computerSelection = computerPlay();
-    playRound(playerSelection, computerSelection);
-    displayScore();
+  if (maximumScore >= numberOfRounds) {
     maximumScore = calculateMaximumScore(playerScore, computerScore);
   }
   displayWinner();
 }
 
 function displayWelcomeMessage() {
-  console.log('Welcome to Rock, Paper, Scissors.');
-  console.log(`The first player to win ${numberOfRounds} rounds wins the game.`)
+  displayedResult.textContent = 'Welcome to Rock, Paper, Scissors.';
+  displayedResult.textContent = `The first player to win ${numberOfRounds} rounds wins the game.`;
+  displayedResult.textContent = 'Welcome to Rock, Paper, Scissors.'
 }
 
 function displayScore() {
   console.log(`Your score: ${playerScore}`);
   console.log(`Computer score: ${computerScore}`);
+  displayedResult.textContent = `Your score: ${playerScore}. Computer score: ${computerScore}`;
 }
 
 function displayWinner() {
@@ -42,7 +45,31 @@ function calculateMaximumScore(playerScore, computerScore) {
 
 function playRound(playerSelection, computerSelection) {
   let winner = determineWinner(playerSelection, computerSelection);
-  console.log(displayRoundWinner(winner, playerSelection, computerSelection));
+  displayRoundWinner(winner, playerSelection, computerSelection);
+  if (playerScore >= numberOfRounds || computerScore >= numberOfRounds) {
+    displayWinner(playerScore, computerScore);
+    // showRestartButton();
+    // resetGame();
+  }
+}
+
+function displayWinner(playerScore, computerScore) {
+  if (playerScore > computerScore) {
+    const winnerLabel = document.createElement('div');
+    winnerLabel.textContent = 'Congratulations! :) You won the game!';
+    displayedResult.appendChild(winnerLabel);
+  } else {
+    const winnerLabel = document.createElement('div');
+    winnerLabel.textContent = 'Too bad! :( Computer won the game';
+    displayedResult.appendChild(winnerLabel);
+  }
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  displayedPlayerScore.innerHTML = playerScore;
+  displayedComputerScore.innerHTML = playerScore;
 }
 
 function determineWinner(playerSelection, computerSelection) {
@@ -52,22 +79,24 @@ function determineWinner(playerSelection, computerSelection) {
   if ((playerMove === 'rock' && computerMove === 'scissors') || (playerMove === 'paper' && computerMove === 'rock') || (playerMove === 'scissors' && computerMove === 'paper')) {
     winner = 'Player';
     playerScore++;
+    displayedPlayerScore.innerHTML = playerScore;
   } else if (playerMove === computerMove) {
     winner = 'Draw';
   } else {
     winner = 'Computer';
     computerScore++;
+    displayedComputerScore.innerHTML = computerScore;
   }
   return winner;
 }
 
 function displayRoundWinner(winner, playerSelection, computerSelection) {
   if (winner === 'Player') {
-    return `You picked ${playerSelection}. Computer picked ${computerSelection}.\nYou Win! ${playerSelection} beats ${computerSelection}`;
+    displayedResult.textContent = `You picked ${playerSelection}. Computer picked ${computerSelection}. You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (winner === 'Computer') {
-    return `You picked ${playerSelection}. Computer picked ${computerSelection}.\nYou Lose! ${computerSelection} beats ${playerSelection}`;
+    displayedResult.textContent = `You picked ${playerSelection}. Computer picked ${computerSelection}.\nYou Lose! ${computerSelection} beats ${playerSelection}`;
   } else {
-    return `You picked ${playerSelection}. Computer picked ${computerSelection}.\nIt\'s a draw! You both picked ${playerSelection}.`
+    displayedResult.textContent = `You picked ${playerSelection}. Computer picked ${computerSelection}.\nIt\'s a draw! You both picked ${playerSelection}.`
   }
 }
 
@@ -82,16 +111,18 @@ function computerPlay() {
   }
 }
 
-function playerPlay() {
-  let playerSelection = prompt('Enter your move (Rock, Paper or Scissors):');
-  let standardizedPlayerSelection = ''
-  if (playerSelection.length > 0) {
-    standardizedPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  }
-  while (!(['Rock', 'Paper', 'Scissors'].includes(standardizedPlayerSelection))) {
-    playerSelection = prompt('Wrong entry. Please enter one of these values: Rock, Paper, Scissors');
-    standardizedPlayerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-  }
-  console.log(standardizedPlayerSelection);
-  return standardizedPlayerSelection;
-}
+let rockButton = document.querySelector('#rock');
+let paperButton = document.querySelector('#paper');
+let scissorsButton = document.querySelector('#scissors');
+
+rockButton.addEventListener('click', () => {
+  playRound('rock', computerPlay());
+});
+
+paperButton.addEventListener('click', () => {
+  playRound('paper', computerPlay());
+});
+
+scissorsButton.addEventListener('click', () => {
+  playRound('scissors', computerPlay());
+});
